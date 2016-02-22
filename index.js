32d63f37afd2809edb83ff4e211af9e8de54e88c
Phoenix09 <Phoenix09@users.noreply.github.com>
2016-02-22 17:13:55 +0000

    Add preferences

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var WindowUtils = require("sdk/window/utils");
 var windows = require("sdk/windows").browserWindows;
 var { viewFor } = require("sdk/view/core");
+let prefs = require("sdk/simple-prefs").prefs;
 
 exports.main = function (options, callbacks) {
 	if (options.loadReason == "startup") {
+		if (!prefs["startup"]) return;
 		for (let window of windows) {
 			var chromeWindow = viewFor(window);
 			if (WindowUtils.isBrowser(chromeWindow) && !chromeWindow.fullScreen) {
@@ -14,6 +16,7 @@ exports.main = function (options, callbacks) {
 };
 
 windows.on("open", function(browserWindow) {
+	if (!prefs["new"]) return;
 	var chromeWindow = viewFor(browserWindow);
 	if (WindowUtils.isBrowser(chromeWindow) && !chromeWindow.fullScreen) {
 		chromeWindow.BrowserFullScreen();
