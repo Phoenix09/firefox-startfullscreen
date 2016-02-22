7054f3628fffb0b1afbb0cebf9b3360c59393131
Phoenix09 <Phoenix09@users.noreply.github.com>
2016-02-22 15:11:31 +0000

    Run on all windows at startup

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ var WindowUtils = require("sdk/window/utils");
 var windows = require("sdk/windows").browserWindows;
 var { viewFor } = require("sdk/view/core");
 
+exports.main = function (options, callbacks) {
+	if (options.loadReason == "startup") {
+		for (let window of windows) {
+			var chromeWindow = viewFor(window);
+			if (WindowUtils.isBrowser(chromeWindow) && !chromeWindow.fullScreen) {
+				chromeWindow.BrowserFullScreen();
+			}
+		}
+	}
+};
+
 windows.on("open", function(browserWindow) {
 	var chromeWindow = viewFor(browserWindow);
 	if (WindowUtils.isBrowser(chromeWindow) && !chromeWindow.fullScreen) {
